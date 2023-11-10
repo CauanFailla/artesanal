9abcdde6fbf527454a35db0e3b5c30835a96a611
cauanfailla <101191360+CauanFailla@users.noreply.github.com>
2023-11-10 08:45:25 -0300

    att

--- scripts/encomenda.js
+++ scripts/encomenda.js
@@ -1,2 +1,23 @@
 import * as headerDef from './contructDef.js'
 import * as scrollMec from './mecanicaScroll.js'
+class Alimentos {
+    constructor(ingredientes) {
+        this.ingredientes = ingredientes
+    }
+}
+let alimentosVisual = [...document.querySelectorAll('#ialimentos option')]
+let alimentoAtual
+alimentosVisual.map((a)=>{
+    a.addEventListener('click',(evt)=>{
+        switch (evt.target.innerHTML) {
+            case 'Linguiça toscana':
+                alimentoAtual = new Alimentos(['pimenta','tomate','limão']) 
+                break;
+        
+            default:
+                break;
+        }
+        
+        
+    })
+})
